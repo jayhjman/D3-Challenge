f61376b4581fad4fc8fa6d904694320013cab490
Jay Hastings <jayhjman@gmail.com>
2021-01-15 17:56:01 -0800

    added axis

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,6 +1,33 @@
 // declare the census data
 var censusData = [];
 
+// Define SVG area dimensions
+var svgWidth = 850;
+var svgHeight = 500;
+
+// Define the chart's margins as an object
+var margin = {
+  top: 60,
+  right: 60,
+  bottom: 60,
+  left: 60,
+};
+
+// Select body, append SVG area to it, and set its dimensions
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append a group area, then set its margins
+var chartGroup = svg
+  .append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - margin.left - margin.right;
+var chartHeight = svgHeight - margin.top - margin.bottom;
 //
 // Initialize the application by reading the data and then
 // call the callback to initialize the graphs
@@ -32,6 +59,32 @@ function initApp(callback) {
 function init() {
   console.log("init()");
   console.log(censusData);
+
+  // d3.extent returns the an array containing the min and
+  // max values for the property specified
+  var xLinearScale = d3
+    .scaleLinear()
+    .domain(d3.extent(censusData, (data) => data.poverty))
+    .range([0, chartWidth]);
+
+  // Configure a linear scale with a range between the chartHeight and 0
+  var yLinearScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(censusData, (data) => data.healthcare)])
+    .range([chartHeight, 0]);
+
+  // Create two new functions passing the scales in as arguments
+  // These will be used to create the chart's axes
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+
+  // Draw the axes
+  chartGroup.append("g").call(leftAxis);
+
+  chartGroup
+    .append("g")
+    .attr("transform", `translate(0, ${chartHeight})`)
+    .call(bottomAxis);
 }
 
 initApp(init);
